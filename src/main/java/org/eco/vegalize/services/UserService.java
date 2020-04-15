07c1c969548befc06d46178930d13723fea8b8c3
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-04-15 17:36:33 -0300

    Create sabe user method

--- src/main/java/org/eco/vegalize/services/UserService.java
+++ src/main/java/org/eco/vegalize/services/UserService.java
@@ -1,5 +1,6 @@
 package org.eco.vegalize.services;
 
+import org.eco.vegalize.models.User;
 import org.eco.vegalize.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public User saveUser(User user){
+        user = userRepository.save(user);
+        return user;
+    }
     
 }
