182a9131dcadc3e96afb8a4e78d15ae57a8d1664
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-04-15 16:59:45 -0300

    Create initial project struct

---
+++ src/main/java/org/eco/vegalize/VegalizeApplication.java
@@ -0,0 +1,13 @@
+package org.eco.vegalize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegalizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegalizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eco/vegalize/VegalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eco.vegalize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
