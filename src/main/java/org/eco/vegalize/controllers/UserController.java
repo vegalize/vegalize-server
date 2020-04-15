cd99b843b797f8811fd9cc2c5d2da4f48a1b8aab
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-04-15 17:46:07 -0300

    Create register user route

--- src/main/java/org/eco/vegalize/controllers/UserController.java
+++ src/main/java/org/eco/vegalize/controllers/UserController.java
@@ -1,4 +1,26 @@
 package org.eco.vegalize.controllers;
 
+import org.eco.vegalize.models.User;
+import org.eco.vegalize.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody @Valid User user){
+        user = userService.saveUser(user);
+        return ResponseEntity.status(201).body(user);
+    }
 }
