cb6e2d1e676f80d81e50f90040d8e2dbea1a4f56
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-04-23 18:26:58 -0300

    Create Product Repository

---
+++ src/main/java/org/eco/vegalize/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.eco.vegalize.repositories;
+
+import org.eco.vegalize.models.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    
+}
