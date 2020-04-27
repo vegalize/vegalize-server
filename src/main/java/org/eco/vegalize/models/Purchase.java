9ecabeec34fc67dd2dcd22b91f22500b04cd30fe
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-04-27 15:25:43 -0300

    Create attributes in Purchases class

--- src/main/java/org/eco/vegalize/models/Purchase.java
+++ src/main/java/org/eco/vegalize/models/Purchase.java
@@ -1,4 +1,66 @@
 package org.eco.vegalize.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToMany
+    private List<Product> items;
+
+    @ManyToOne
+    private User buyer;
+
+    @ManyToOne
+    private User provider;
+
+    private double amout;
+
+    public Purchase() {
+    }
+
+    public double getAmout() {
+        return amout;
+    }
+
+    public void setAmout(double amout) {
+        this.amout = amout;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Product> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Product> items) {
+        this.items = items;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public User getProvider() {
+        return provider;
+    }
+
+    public void setProvider(User provider) {
+        this.provider = provider;
+    }
 }
