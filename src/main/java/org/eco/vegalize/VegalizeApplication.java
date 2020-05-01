0c93ecf69729df7c2e505fcf499a30008db7d4ac
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-05-01 16:03:30 -0300

    Create Amazon S3 Integration

--- src/main/java/org/eco/vegalize/VegalizeApplication.java
+++ src/main/java/org/eco/vegalize/VegalizeApplication.java
@@ -1,13 +1,20 @@
 package org.eco.vegalize;
 
+import org.eco.vegalize.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
-public class VegalizeApplication {
+@SpringBootApplication(exclude = ContextStackAutoConfiguration.class)
+public class VegalizeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VegalizeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
---
+++ src/main/java/org/eco/vegalize/enums/PaymentStatus.java
@@ -0,0 +1,4 @@
+package org.eco.vegalize.enums;
+
+public class PaymentStatus {
+}
